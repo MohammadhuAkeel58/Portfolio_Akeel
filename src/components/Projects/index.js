2726bda20a3a99e5c17846acfdc06ccc08d29167
Mohamed Akeel <akeelaslamcse@gmail.com>
2024-08-19 14:29:28 +0530

    second changes

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -58,7 +58,7 @@ const Projects = ({ openModal, setOpenModal }) => {
               />
             ))}
           {projects
-            .filter((item) => item.category == toggle)
+            .filter((item) => item.category === toggle)
             .map((project) => (
               <ProjectCard
                 project={project}
